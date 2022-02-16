f21002205dccc96bdc1b38fd9041860e0dfe2c7e
SuanNyan <94887629+SuanNyan@users.noreply.github.com>
2022-02-16 16:43:31 +0630

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,33 @@
-<?php  
-for($i=11; $i>=1; $i--)  
-{  
-if($i%2 != 0)  
-{  
-for($j=11; $j>=$i; $j--)  
-{  
-echo " * ";  
-}  
-echo "<br>";  
-}  
-}  
-for($i=2; $i<=11; $i++)  
-{  
- if($i%2 != 0)  
-{  
- for($j=11; $j>=$i; $j--)  
-{  
-echo "  *  ";  
-}  
-echo "<br>";  
-}  
-}  
-?> 
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chess Board</title>
+</head>
+<body>
+<h1>Tutorial 1 (Chess board in php)</h1>
+   <table width="270px" cellspacing="0px" cellpadding="0px" border="1px">
+   <!-- cell 270px wide (8 columns x 60px) -->
+      <?php
+      for($row=1;$row<=8;$row++)
+	  {
+          echo "<tr>";
+          for($column=1;$column<=8;$column++)
+		  {
+          $total=$row+$column;
+          if($total%2==0)
+		  {
+          echo "<td height=25px width=25px bgcolor=#FFFFFF></td>";
+          }
+		  else
+		  {
+          echo "<td height=25px width=25px bgcolor=#000000></td>";
+          }
+          }
+          echo "</tr>";
+    }
+          ?>
+  </table>
+</body>
+</html>
