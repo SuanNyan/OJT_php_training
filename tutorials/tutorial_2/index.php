3a1bebcc0809f15827616ebe86e118077ab2e8d9
SuanNyan <94887629+SuanNyan@users.noreply.github.com>
2022-02-17 16:48:25 +0630

    Update index.php

--- tutorials/tutorial_2/index.php
+++ tutorials/tutorial_2/index.php
@@ -1,24 +1,23 @@
-<?php  
-for($i=11; $i>=1; $i--)  
-{  
-if($i%2 != 0)  
-{  
-for($j=11; $j>=$i; $j--)  
-{  
-echo " * ";  
-}  
-echo "<br>";  
-}  
-}  
-for($i=2; $i<=11; $i++)  
-{  
- if($i%2 != 0)  
-{  
- for($j=11; $j>=$i; $j--)  
-{  
-echo "  *  ";  
-}  
-echo "<br>";  
-}  
-}  
-?> 
+<?php
+echo "<pre>";
+for ($i = 1; $i < 6; $i++) {
+    for ($j = $i; $j < 8; $j++) {
+        echo "  ";
+    }
+    for ($j = 2 * $i - 1; $j > 0; $j--) {
+        echo(" *");
+    }
+    echo "<br>";
+}
+$k = 8;
+for ($l = 6; $l > 0; $l--) {
+    for ($m = $k - $l; $m > 0; $m--) {
+        echo "  ";
+    }
+    for ($m = 2 * $l - 1; $m > 0; $m--) {
+        echo(" *");
+    }
+    echo "<br>";
+}
+echo "</pre>";
+?>
