b3f179a84eecf7ecca4a2b64eb220a649e0595b5
SuanNyan <94887629+SuanNyan@users.noreply.github.com>
2022-02-23 15:07:50 +0630

    Update userinfoconnect.php

--- tutorials/tutorial_9/userinfoconnect.php
+++ tutorials/tutorial_9/userinfoconnect.php
@@ -10,5 +10,4 @@ $connection = mysqli_connect($servername, $username, $password,$database);
 if ($connection->connect_error) {
   die("Connection failed: " . $connection->connect_error);
 }
-//echo "Connected successfully";
 ?>
